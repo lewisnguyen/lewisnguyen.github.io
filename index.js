590f0abf31651447e2a23c8918d28889a6c97b39
lewisnguyen <lewis.nguyen.36@gmail.com>
2017-07-31 06:55:06 +0700

    Make change to the index.js!

--- index.js
+++ index.js
@@ -15,9 +15,14 @@ console.log(PI);
 const name = prompt("Enter your name: ");
 alert("Hello, " + name);
 */
+/**
+* Function that calculate the VAT free
+*/
 let calculate_VAT = function(_price) {
-	let _total = _price * 20.6;
-	let _VAT = _total/100;
+	let _total = _price * 20.6; // Multiply _price by 20.6 times
+	console.log("The price when It is multiplied by 20.6 times: " + _total); // Log
+	let _VAT = _total/100; // Calculate VAT
+	console.log("The final VAT fee: " + _VAT); // Log
 	return _VAT;
 }
 let calculate_price = function(_price) {
@@ -42,3 +47,37 @@ let swap_number = function(_number3, _number4) {
 swap_number(_number1,_number2);
 console.log(_number1);
 console.log(_number2);
+/*
+const _number = Number(prompt("Enter the number: "));
+if (_number > 0) {
+	console.log(`${_number} is positive`);
+}
+*/
+let the_next_day = function(_day_name) {
+	switch(_day_name) {
+		case "monday":
+		alert("The next day is Tuesday");
+		break;
+		case "tuesday":
+		alert("The next day is Wednesday");
+		break;
+		default:
+		alert("I can't understand you");
+	}
+}
+/*
+const _day_name = prompt("Enter the day's name: ");
+the_next_day(_day_name);
+*/
+let compare_number = function(_number1,_number2) {
+	if (_number1 > _number2) {
+		alert("The number1 is greater than the number2");
+	} else if (_number1 < _number2) {
+		alert("The number2 is greater than the number1");
+	} else {
+		alert("The number1 and the number2 are equal!");
+	}
+}
+const _number_1 = prompt("Enter the number 1: ");
+const _number_2 = prompt("Enter the number 2: ");
+compare_number(_number_1,_number_2);
